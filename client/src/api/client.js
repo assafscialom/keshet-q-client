0c3deba6bab3cc70109344203842472f50c53ecb
Sol Scialom <sol@scidan.co.uk>
2026-01-06 15:46:22 +0200

    fixing api

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://qclient.iqdesk.xyz/server/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://qclient.iqdesk.xyz/server/public/api',
 });
 
 export default apiClient;
